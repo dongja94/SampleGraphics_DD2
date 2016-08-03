84fa7df77efc3e90d430245e9df737820305a072
Yon Seungik <dongja94@gmail.com>
2016-08-03 17:27:23 +0900

    finger paint

---
+++ app/src/main/java/com/begentgroup/samplegraphics/PathDrawItem.java
@@ -0,0 +1,12 @@
+package com.begentgroup.samplegraphics;
+
+import android.graphics.Paint;
+import android.graphics.Path;
+
+/**
+ * Created by Administrator on 2016-08-03.
+ */
+public class PathDrawItem {
+    Path path;
+    Paint paint;
+}
